d6da5d1abb592215a26b89f5ced3d3fcca892906
RobotAFK <Dakotadunn92@gmail.com>
2021-02-20 02:59:48 +0000

    code cleanup + program will no proceed with a tip value below 10

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 #Welcome screen
 print("Welcome to the tip calculator!")
 #Variables and questions
-bill = input("What was the total bill? $")
-tip = input("What percentage tip would you like to give? 10, 12, or 15? ")
-people = input("How many people to split the bill? ")
-#Converting variables into floats and ints
-bill_float = float(bill)
-tip_int = int(tip)
-people_int = int(people)
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+#If user says below 10, program calls them cheap and line 5 loops.
+while tip < 10:
+  print("Why are you so cheap, please give that person a good tip >:(")
+  tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
 #Calculation
-tip_percent = tip_int / 100
-tip_final = bill_float * tip_percent
-bill_final = bill_float + tip_final
-people_final = bill_final / people_int
+tip_percent = tip / 100
+tip_final = bill * tip_percent
+bill_final = bill + tip_final
+people_final = bill_final / people
 grand_total = round(people_final, 2)
 #Grand total 
 print(f"Each person should pay ${grand_total}")
