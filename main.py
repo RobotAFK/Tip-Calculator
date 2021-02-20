0d4ee8212172c4885e0d1f4975abe26edf69e534
RobotAFK <Dakotadunn92@gmail.com>
2021-02-20 03:04:04 +0000

    Removed the tip line asking for a value of 10, 12, or 15.

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 print("Welcome to the tip calculator!")
 #Variables and questions
 bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+tip = int(input("What percentage tip would you like to give? "))
 #If user says below 10, program calls them cheap and line 5 loops.
 while tip < 10:
   print("Why are you so cheap, please give that person a good tip >:(")
-  tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+  tip = int(input("What percentage tip would you like to give? "))
 people = int(input("How many people to split the bill? "))
 #Calculation
 tip_percent = tip / 100
